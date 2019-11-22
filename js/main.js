b06d99f1daf249bf6ff2b4871dfa4e4b9ae040b8
Gavin Bowen <gavinbowen@pop-os.localdomain>
2019-11-21 16:02:19 -0800

    init

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(window.innerHeight, window.innerWidth);
 }
 
 function draw() {
-    background(255);
-    text("put your p5.js code here",10, frameCount % height);
-}
-
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    if(mouseIsPressed) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+    ellipse(mouseX, mouseY, 80,80);
 }
